ccb86101212e9089cc290e75deb41415ec78d166
shahb <malikshahbazzafar@gmail.com>
2023-01-08 03:19:04 +0500

    ThirdComment

---
+++ app/src/main/java/com/example/commoncode/data/adapters/RecyclerViewExtraFunctions.kt
@@ -0,0 +1,12 @@
+package com.example.commoncode.data.adapters
+
+class RecyclerViewExtraFunctions {
+
+//    private fun deleteItem(position: Int) {
+//        mCustomList.removeAt(position)
+//        notifyItemRemoved(position)
+//        notifyItemRangeChanged(position, mCustomList.size)
+//        //holder.itemView.setVisibility(View.GONE);
+//    }
+
+}
