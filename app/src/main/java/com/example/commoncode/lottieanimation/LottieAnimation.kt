7e8d78f8063710cd89f5b88fbf1d0a240365f297
malikshahbazzafar <shahbazzafar>
2023-07-04 17:45:46 +0500

    Add changeColorLottieAnimation function

---
+++ app/src/main/java/com/example/commoncode/lottieanimation/LottieAnimation.kt
@@ -0,0 +1,24 @@
+package com.example.commoncode.lottieanimation
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+
+private fun changeColorLottieAnimation(){
+//    val animationView = findViewById<LottieAnimationView>(R.id.animation_view)
+//    var mCallback: LottieValueCallback<Int?>
+//    animationView.addLottieOnCompositionLoadedListener {
+//        mCallback = LottieValueCallback<Int?>()
+//        mCallback.setValue(Color.RED)
+//        animationView.addValueCallback(
+//            KeyPath("**"),
+//            LottieProperty.COLOR,
+//            mCallback
+//        )
+//    }
+//
+//    animationView.addValueCallback(
+//        KeyPath( "**"),
+//        LottieProperty.COLOR_FILTER
+//    ) { PorterDuffColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP) }
+}
