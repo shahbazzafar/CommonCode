8313968fae5bcabe189cf9a330c679a061fb448c
malikshahbazzafar <shahbazzafar>
2023-07-11 16:19:32 +0500

    BuildConfig Issue update

--- app/src/main/java/com/example/commoncode/issues/NormalCodeIssues.kt
+++ app/src/main/java/com/example/commoncode/issues/NormalCodeIssues.kt
@@ -4,6 +4,5 @@ package com.example.commoncode.issues
  * BuildConfig Issue
  */
 
-//add below line of code in gradle.properties
-//
+//add below line of code in gradle.properties its needed if project gradle version is 8.0 or above
 //android.defaults.buildfeatures.buildconfig=true
