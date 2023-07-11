03f6559939052025bf8c68c74f354aab593de56c
malikshahbazzafar <shahbazzafar>
2023-07-11 15:56:23 +0500

    BuildConfig Issue

--- app/src/main/java/com/example/commoncode/beforeproduction/NeedToCheckAllTaskBeforeProduction.kt
+++ app/src/main/java/com/example/commoncode/beforeproduction/NeedToCheckAllTaskBeforeProduction.kt
@@ -95,3 +95,22 @@ package com.example.commoncode.beforeproduction
 //        outputFileName = apkName
 //    }
 //}
+
+/**
+ * ProGuard rules
+ */
+//-keep public class * implements com.bumptech.glide.module.GlideModule
+//-keep class * extends com.bumptech.glide.module.AppGlideModule {
+//    <init>(...);
+//}
+//-keep public enum com.bumptech.glide.load.ImageHeaderParser$** {
+//    **[] $VALUES;
+//    public *;
+//}
+//-keep class com.bumptech.glide.load.data.ParcelFileDescriptorRewinder$InternalRewinder {
+//    *** rewind();
+//}
+//# Uncomment for DexGuard only
+//#-keepresourcexmlelements manifest/application/meta-data@value=GlideModule
+
+//https://github.com/bumptech/glide/blob/master/library/proguard-rules.txt
---
+++ app/src/main/java/com/example/commoncode/issues/NormalCodeIssues.kt
@@ -0,0 +1,9 @@
+package com.example.commoncode.issues
+
+/**
+ * BuildConfig Issue
+ */
+
+//add below line of code in gradle.properties
+//
+//android.defaults.buildfeatures.buildconfig=true
