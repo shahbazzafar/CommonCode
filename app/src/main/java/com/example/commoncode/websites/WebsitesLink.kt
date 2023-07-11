136f7963c00ebc612852c99eb0554726cde44946
malikshahbazzafar <shahbazzafar>
2023-07-11 16:42:49 +0500

    add websites class

--- app/src/main/java/com/example/commoncode/websites/WebsitesLink.kt
+++ app/src/main/java/com/example/commoncode/websites/WebsitesLink.kt
@@ -1,3 +1,11 @@
 package com.example.commoncode.websites
 
+/**
+ * Android Gradle
+ */
 //https://developer.android.com/build/migrate-to-ksp
+//https://developer.android.com/build/releases/gradle-plugin
+
+/**
+ * Others
+ */
