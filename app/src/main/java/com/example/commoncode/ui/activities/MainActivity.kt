6956290bd7a965e69852bc3bd395d4878cd8386e
malikshahbazzafar <shahbazzafar>
2023-07-11 14:30:42 +0500

    Add MultipleRecyclerview Adapter update

--- app/src/main/java/com/example/commoncode/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/commoncode/ui/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.commoncode.ui.activities
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.commoncode.R
 import com.example.commoncode.domain.utils.Extensions.showToast
 
@@ -29,5 +30,29 @@ class MainActivity : AppCompatActivity() {
 //        binding.recyclerView.adapter = adapter
 //        binding.recyclerView.layoutManager = LinearLayoutManager(this)
 //        adapter.setData(listOf(Person(1,"Shahbaz",33)))
+//    }
+
+    /**
+     * For Multiple RecyclerViews
+     */
+
+//    val initRecycler = {
+//        binding.apply {
+//            mainLayoutMain.recyclerview.adapter = menuAdapter
+//            val layoutManager = mainLayoutMain.recyclerview.layoutManager as GridLayoutManager
+//            layoutManager.spanSizeLookup =
+//                object : GridLayoutManager.SpanSizeLookup() {
+//                    override fun getSpanSize(position: Int): Int {
+//                        return when (position) {
+//                            0 -> 2
+//                            menuAdapter.currentList.size - 1 -> 2
+//                            1 -> 2
+//                            menuAdapter.currentList.size - 2 -> 2
+//                            else -> 1
+//                        }
+//                    }
+//                }
+//            menuAdapter.submitList(getData())
+//        }
 //    }
 }
